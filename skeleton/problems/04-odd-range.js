ab45c9a95804466ca338998b7f21d61fd65d08d2
Wardo <ward.keegan@gmail.com>
2021-02-15 16:15:55 -0500

    4th and 5th

--- skeleton/problems/04-odd-range.js
+++ skeleton/problems/04-odd-range.js
@@ -9,8 +9,15 @@ oddRange(6); // => [ 1, 3, 5 ]
 *******************************************************************************/
 
 function oddRange(end) {
-  // your code here...
+  let odds = [];
+  for(let i = 1; i <= end; i += 2){
+    odds.push(i);
+  }
+  return odds;
 }
+
+console.log(oddRange(13)); // => [ 1, 3, 5, 7, 9, 11, 13 ]
+console.log(oddRange(6)); // => [ 1, 3, 5 ]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
 module.exports = oddRange;
